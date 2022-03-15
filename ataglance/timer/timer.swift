f43080c67a434b59ae96016ac0cec7d7e7a008a0
Yao Lin <yao.lin332@csu.fullerton.edu>
2022-03-15 09:47:44 -0700

    create all the files for timer feature

---
+++ ataglance/timer/timer.swift
@@ -0,0 +1,8 @@
+//
+//  timer.swift
+//  ataglance
+//
+//  Created by y41 on 3/15/22.
+//
+
+import Foundation
---
+++ ataglance/timer/timerVC.swift
@@ -0,0 +1,29 @@
+//
+//  timerVC.swift
+//  ataglance
+//
+//  Created by y41 on 3/15/22.
+//
+
+import UIKit
+
+class timerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

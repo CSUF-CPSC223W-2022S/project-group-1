fd78a27a12fb75ca884ddd8f596763d3c24c03ef
Brian Escobar <brianescobar523@gmail.com>
2022-03-23 23:00:27 -0700

    new branch cause of issues

--- ataglance/Notifications.swift
+++ ataglance/Notifications.swift
@@ -11,19 +11,20 @@ class Notifications {
     var notifMessages: [String] = []
     var notifTime: [String] = []
     var notifNumber: Int
+    var test: Int
     
     init() {
         notifNumber = 0
         notifMessages = []
         notifTime = []
     }
-    
+
     func addNotification(time: String, message: String) {
         notifMessages.append(message)
         notifTime.append(time)
         notifNumber += 1
     }
-    
+
     func deleteNotification(time: String, message: String) {
         let indexVal = getMessageIndex(messageTarget: message)
         if indexVal != -1 {
@@ -33,12 +34,12 @@ class Notifications {
             print("Notification does not exist")
         }
     }
-    
+
     func changeNotificationMessage(time: String, newMessage: String) {
         let index = getTimeIndex(TimeTarget: time)
         notifMessages[index] = newMessage
     }
-    
+
     func changeNotificationTime(newTime: String, message: String) {
         let index = getMessageIndex(messageTarget: message)
         notifTime[index] = newTime
@@ -48,7 +49,7 @@ class Notifications {
         let index = getTimeIndex(TimeTarget: time)
         print(" \(notifMessages[index])")
     }
-    
+
     func getMessageIndex(messageTarget: String) -> Int {
         for (index, messages) in notifMessages.enumerated() {
             if messages == messageTarget {
@@ -57,7 +58,7 @@ class Notifications {
         }
         return -1
     }
-    
+
     func getTimeIndex(TimeTarget: String) -> Int {
         for (index, time) in notifTime.enumerated() {
             if time == TimeTarget {

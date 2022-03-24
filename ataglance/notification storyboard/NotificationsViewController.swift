318e65b6dc74d41225eb91965524d18452a6c240
Brian Escobar <brianescobar523@gmail.com>
2022-03-24 09:42:35 -0700

    about to merge

--- ataglance/notification storyboard/NotificationsViewController.swift
+++ ataglance/notification storyboard/NotificationsViewController.swift
@@ -6,13 +6,50 @@
 //
 
 import UIKit
+import UserNotifications
 
 class NotificationsViewController: UIViewController {
 
+    @IBOutlet var datepicker: UIDatePicker!
+    @IBOutlet var messageTextField: UITextField!
+    
+    @IBOutlet var createbutton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
+        // Step 1: Ask for permission
+        let center = UNUserNotificationCenter.current()
+        
+        center.requestAuthorization(options: [.alert, .sound]) { (granted, error) in
+        }
+        
+        // Step 2: Create the notification content
+        let content = UNMutableNotificationContent()
+        content.title = "Hey I'm a notification!"
+        content.body = "Look at me!"
+        
+        // Step 3: Create the notification trigger
+        let date = Date().addingTimeInterval(10)
+        
+        let dateComponents = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        // Step 4: Create the request
+        
+        let uuidString = UUID().uuidString
+        
+        let request = UNNotificationRequest(identifier: uuidString, content: content, trigger: trigger)
+        
+        // Step 5: Register the request
+        center.add(request) { (error) in
+            // Check the error parameter and handle any errors
+        }
+        
+        
     }
     
 

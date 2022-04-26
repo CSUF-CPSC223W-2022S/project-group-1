ac6560d5f5060df42f5c85351c7a730398ea320f
Luis Alvarado <luisalvarado@csu.fullerton.edu>
2022-04-25 18:56:24 -0700

    Finished with todoList Feature. I need to work on Unit testing now

---
+++ ataglance/ToDoListFeature/todoListStructure.swift
@@ -0,0 +1,14 @@
+//
+//  todoListStructure.swift
+//  ataglance
+//
+//  Created by Luis Alvarado on 4/11/22.
+//
+
+import Foundation
+
+struct todoItems: Codable {
+    var name: String
+    var date: Date
+    var notes: String
+}
